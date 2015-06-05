bccb957d3a61f35d14adc3878004c1101c59a986
shuaizhou487 <zhoushuai@softwarecomp1.hci-llc.local>
2015-06-05 15:44:12 -0400

    a

--- js/myjs.js
+++ js/myjs.js
@@ -1,10 +1,10 @@
+
 $(document).ready(function(){
 	$("#go-top").hide();
 	        $(window).scroll(function(){
 	            	$(window).scroll(function(){
 	            		if($(window).scrollTop()>50){
 	            			$("#go-top").fadeIn(500);
-	            			$(".header").addClass('.scrolled');
 	            		}
 	            	    else{
 	            	    	$("#go-top").fadeOut(500);
@@ -12,8 +12,25 @@ $(document).ready(function(){
 	            	});
 	        });
 
+
 	        $("#go-top a").click(function(){
 	        	$("html,body").animate({scrollTop:0},800);
 	        	return false;
 	         });
+})
+
+
+$(function(){
+    $(window).scroll(function(){
+             var tops = $(".header"); 
+             var scroH = $(document).scrollTop(); 
+             tops.each(function(){ 
+                var top = $(this).offset().top; 
+                    if (scroH > 0 ) { 
+                    $(".header").addClass('scrolled') ;
+                 } else { 
+                     $(".header").removeClass('scrolled') ;
+             } 
+        })  
+    })
 })
