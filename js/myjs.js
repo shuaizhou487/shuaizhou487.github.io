366857f6439304e9df97b0234408bf32a1142108
shuaizhou487 <zhoushuai@Zhous-MacBook-Pro.local>
2015-09-28 23:29:06 -0400

    a

--- js/myjs.js
+++ js/myjs.js
@@ -20,12 +20,11 @@ $(document).ready(function(){
 })
 
 $(document).ready(function() {
-			$('#fullpage').fullpage({
-				continuousVertical: true,
+	$('#fullpage').fullpage({
+		        continuousVertical: true,
 				anchors: ['page1', 'page2', 'page3', 'page4', 'page5', 'page6', 'page7', 'page8', 'page9', 'page10','page11','page12','page13'],
 				menu: '#menu',
 				'navigation':true,
 				scrollingSpeed: 500,
-
-			});
+	});
 });
