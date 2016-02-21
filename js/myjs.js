395fb99e4ae679f58713c0c1ed85a7c8533d143c
shuaizhou487 <zhoushuai@Zhous-MacBook-Pro.local>
2016-02-21 20:43:52 +0100

    a

--- js/myjs.js
+++ js/myjs.js
@@ -3,7 +3,6 @@ $(document).ready(function() {
 		        continuousVertical: true,
 				anchors: ['page1', 'page2', 'page3', 'page4', 'page5', 'page6', 'page7', 'page8', 'page9', 'page10','page11','page12','page13'],
 				menu: '#menu',
-				'navigation':true,
-				scrollingSpeed: 500,
+				'navigation':true
 	});
 });
